0838ed7486843fdad8b5ee32e520acfc67f2dd29
Saral Gupta <saralgup.taxtc@gmail.com>
2026-01-27 21:05:00 +0530

    new code

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+
+app.get("/", (req, res) => {
+  res.json({message: "This route is working!"})
+})
+
+app.listen(PORT, () => {
+  console.log(`Sever is listening on port ${PORT}`)
+})
